27039e8e4fc9103f0c6dbab515ae15ada6fbccac
Heizenberg <87001781+ufs8889@users.noreply.github.com>
2023-11-26 20:58:44 +0500

    Update get_all_urls(makro).py

--- get_all_urls(makro).py
+++ get_all_urls(makro).py
@@ -3,27 +3,26 @@ from bs4 import BeautifulSoup
 import itertools
 import time
 start = time.time()
-get_data = lambda url: BeautifulSoup(((requests.get(url)).text),"html.parser") # [BeautifulSoup(html, 'html.parser') 
+get_data = lambda url: BeautifulSoup(((requests.get(url)).text),"html.parser") 
 product_url = "https://makromarket.uz/categories/sveci"
 soup = get_data(product_url)   
-#get_product_urls = [link.get('href') for link in soup.find_all('a') if link.get('href').startswith("https://makromarket.uz/categories")]
+
 get_product_urls = ["https://makromarket.uz/categories/sveci","https://makromarket.uz/categories/kartofel","https://makromarket.uz/categories/konservaciya-specii-i-dobavki","https://makromarket.uz/categories/zamorozennaya-gotovaya-produkciya"]
 
 with_pagination = []
 without_pagination = []
 filter_by_pagionation_or_not = [with_pagination.append(x) if get_data(x).find("ul", class_="pagination") else without_pagination.append((x,)) for x in get_product_urls ]
-#print(with_pagination)
-#print(without_pagination)
+
 pagination_page_soup = [get_data(x).find("ul", class_="pagination") for x in with_pagination]
 
 get_all_links = [[x["href"] for x in y.find_all("a")] for y in pagination_page_soup]
-#print(get_all_links)
+
 filter_links = [[x for x in get_all_links[number] if x.startswith("https://makromarket.uz/categories/")] for number in range(len(get_all_links ))]
 
 duplicate_removed_links = [list(dict.fromkeys(filter_links[number])) for number in range(len(filter_links))]
-#print(duplicate_removed_links)
+
 largest_number_of_pagination = [x[-1][-6:].translate({ord(i): None for i in ':/N-?=QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm,.@йцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮСумкг"\n, '}) for x in duplicate_removed_links]
-#print(price_sort)
+
 turn_into_integer = [int(number) for number in largest_number_of_pagination]
 
 pagination_number_list = [[i for i in range(2,number+1)]for number in turn_into_integer]
